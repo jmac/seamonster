1525618f175c7b4882af61343eb8b9e3c0150e6d
John <john@goldengrouse.com>
2010-01-18 17:15:06 +1100

    Added test.php file

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php 
+
+  $_ROOT_DIRECTORY = dirname(__FILE__) . "/";
+  
+  foreach (glob($_ROOT_DIRECTORY . "includes/*.php") as $filename) {
+     if ($filename !== ($_ROOT_DIRECTORY . "includes/core.php")) {
+       require_once($filename);
+     }
+  }
+
+  
+  $b = new Backend();
+  
+  
+  print_r ($b->list_files());
+  
+?>
